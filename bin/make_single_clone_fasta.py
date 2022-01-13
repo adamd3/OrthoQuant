233b760e57056fb025e8e1da8fc009167c9f3318
Adam Dinan <adam1989ie@gmail.com>
2022-01-13 16:18:05 +0000

    switch to pyfaidx

--- bin/make_single_clone_fasta.py
+++ bin/make_single_clone_fasta.py
@@ -4,8 +4,7 @@ import argparse
 import pandas as pd
 import os.path
 import textwrap
-from Bio import SeqIO
-
+from pyfaidx import Fasta
 
 def parse():
     parser = argparse.ArgumentParser()
@@ -27,18 +26,16 @@ def parse():
 def make_fasta(multifasta_file, gene_presence_absence, strain_name):
     csv_data = pd.read_csv(gene_presence_absence, low_memory=False)
     colnames = csv_data.columns.values.tolist()
-    # gene_seq = Fasta(multifasta_file)
+    gene_seq = Fasta(multifasta_file)
     if strain_name in colnames:
         strain_genes = csv_data[strain_name].tolist()
-        records = (r for r in SeqIO.parse(multifasta_file, "fasta") if r.id in strain_genes)
-        # genes_present = [gene for gene in strain_genes if gene in gene_seq.keys()]
+        genes_present = [gene for gene in strain_genes if gene in gene_seq.keys()]
         outf2 = os.path.normpath(strain_name+'.fna')
-        SeqIO.write(records, outf2, "fasta")
-        # f = open(outf2, 'a')
-        # for gene in genes_present:
-        #     seq = '\n'.join(textwrap.wrap(str(gene_seq[gene]), 50))
-        #     f.write(">"+gene+"\n"+seq+"\n")
-        # f.close()
+        f = open(outf2, 'a')
+        for gene in genes_present:
+            seq = '\n'.join(textwrap.wrap(str(gene_seq[gene]), 50))
+            f.write(">"+gene+"\n"+seq+"\n")
+        f.close()
 
 if __name__ == "__main__":
     parse()
